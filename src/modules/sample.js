d8bef5afb321fc10ca77e1e624b6b57bd24de8f0
B H An <anbyunghoon0107@gamil.com>
2021-11-19 16:31:02 +0900

    API 요청 상태 관리

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -17,8 +17,53 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 // thunk 함수를 생성한다
 // thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치 한다.
 
-export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+export const getPost = createAction(GET_POST, id => id);
+export const getUsers = createAction(GET_USERS);
+
+function* getPostSaga(action) {
+    yield put(startLoading(GET_POST)); // 로딩 시작
+    // 파라미터로 action을 받아 오면 액션의 정보를 조회할 수 있습니다.
+    try {
+        // call을 사용하면 Promise를 반환하는 함수를 호출하고, 기다릴 수 있다.
+        // 첫 번째 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인수이다.
+        const post = yield call(api.getPost, action.payload); // api.getPost(action.pay-load)를 의미
+        yield put({
+            type: GET_POST_SUCCESS,
+            payload: post.data
+        });
+    } catch (e) {
+        // try/catch 문을 사용하여 에러도 잡을 수 있다.
+        yield put({
+            type: GET_POST_FAILURE,
+            payload: e,
+            error: true
+        });
+    }
+    yield put(finishLoading(GET_POST)); // 로딩 완료
+}
+
+function* getUsersSaga() {
+    yield put(startLoading(GET_USERS));
+    try {
+        const users = yield call(api.getUsers);
+        yield put({
+            type: GET_USERS_SUCCESS,
+            payload: users.data
+        });
+    } catch (e) {
+        yield put({
+            type: GET_USERS_FAILURE,
+            payload: e,
+            error: true
+        });
+    }
+    yield put(finishLoading(GET_USERS));
+}
+
+export function* sampleSaga() {
+    yield takeLatest(GET_POST, getPostSaga);
+    yield takeLatest(GET_USERS, getUsersSaga);
+}
 
 // 초기 상태를 선언한다.
 // 요청의 로딩 중 상태는 loading 이라는 객체에서 관리한다.
